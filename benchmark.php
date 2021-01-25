9ee3c831bc7c98f38a8a448005ff5322c7ef80e8
Nicalia-Hosting <77512383+Nicalia-Hosting@users.noreply.github.com>
2021-01-25 16:17:49 -0500

    Update benchmark.php
    set ini_set("date.timezone", "UTC");

--- benchmark.php
+++ benchmark.php
@@ -2,6 +2,7 @@
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+ini_set("date.timezone", "UTC");
 /**
  * PHP Script to benchmark PHP and MySQL-Server
  * http://odan.github.io/benchmark-php/
